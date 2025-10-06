68f25e1b0a0272758196e84cfc4bbd1de76f66bf
E.Dhanush <dhanushenugu22052006@gmail.com>
2025-10-06 14:04:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -7,8 +7,6 @@ const deleteBtn = document.getElementById("delete-btn")
 const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"))
 const tabBtn = document.getElementById('tab-btn')
 
-// 1.Check if leadsFromLocalStorage is truthy
-// 2.If so,set myLeads to its value and call renderLeads()
 if(leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
@@ -16,7 +14,6 @@ if(leadsFromLocalStorage) {
 
 
 tabBtn.addEventListener("click", ()=> {
-    // Grab the URL of the current tab
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         myLeads.push(tabs[0].url)
         localStorage.setItem("myLeads",JSON.stringify(myLeads))
@@ -27,7 +24,6 @@ tabBtn.addEventListener("click", ()=> {
 function render(leads) {
     let listItems = ""
     for (let i = 0; i < leads.length; i++) {
-        // listItems += "<li><a target='_blank' href='" + myLeads[i] + "'>" + myLeads[i] + "</a></li>"
         listItems += `
             <li>
                 <a target='_blank' href='${leads[i]}'>
@@ -39,7 +35,6 @@ function render(leads) {
     ulEl.innerHTML = listItems
 }
 
-// when clicked, clear localStorage, myLeads and the DOM
 deleteBtn.addEventListener("dblclick", ()=>{
     localStorage.clear()
     myLeads = []
@@ -49,28 +44,8 @@ deleteBtn.addEventListener("dblclick", ()=>{
 inputBtn.addEventListener("click", ()=> {
     myLeads.push(inputEl.value)
     inputEl.value = ""
-    // save the myLeads array to localStorage
-    // remember JSON.stringify()
     localStorage.setItem("myLeads",JSON.stringify(myLeads))
     render(myLeads)
-    // To verify 
-    // console.log(localStorage.getItem("myLeads"))
 })
 
-
-
-
-// document.createElement() creates a new HTML element(node) in JavaScript-but it dosen't appear on the page until we add it to the DOM
-// or the following method
-    // const li = document.createElement("li")
-    // li.textContent = myLeads[i]
-    // ulEl.append(li)
-
-
-// json means javascript object notation it is used tp send a piece of data from a server to a client(that is to a browser)
-// To save anything in the localStorage it should be in the form of strings not arrays and objects
-// We will use JSON.stringify to save it in the string
-// localStorage.setItem("<key_name>", JSON.stringify(<data>))
-// localStorage.setItem("myLeads", JSON.stringify(myLeads))  it saves in myLeads from myLeads in the form of string
-// JSON.parse() converts a JSON string (text) back into its original JavaScript data type — like an array, object, number, or boolean.
 // Exactly! You use localStorage.getItem() to retrieve (get) data that you previously saved.
